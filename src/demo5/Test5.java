f6bdbc691920654f6175fc58e97895c28fd1f671
valen <valen@DESKTOP-057E61O.lan>
2023-04-09 18:31:19 -0400

    new commit

---
+++ src/demo5/Test5.java
@@ -0,0 +1,15 @@
+package demo5;
+
+public class Test5 {
+	
+	public static void main (String arg ) {
+		
+		System.out.println("Test5");
+	}
+public void name(String name) {
+	
+	System.out.println("Test 5");
+	
+	
+}
+}
